1ab35027ad3fa706419d4516d5b297bf55462573
Colin Whiteford <colinwhiteford516@gmail.com>
2025-11-18 19:11:52 -0500

    Ball Trigger Sound Code

--- Assets/Scripts/Environment/Ball Trigger.cs
+++ Assets/Scripts/Environment/Ball Trigger.cs
@@ -1,4 +1,5 @@
 using System;
+using AudioSystem;
 using CustomNamespace.GenericDatatypes;
 using UnityEngine;
 using UnityEngine.Events;
@@ -7,14 +8,18 @@ public class BallTrigger : MonoBehaviour
 {
     [SerializeField] LayerMask m_triggerLayers;
     [SerializeField] Observer<bool> m_isTriggered;
+    [SerializeField] SoundData m_triggerOn;
+    [SerializeField] SoundData m_triggerOff;
     void OnTriggerEnter2D(Collider2D other)
     {
         if((m_triggerLayers.value & (1 << other.gameObject.layer)) is 0){ return; }
+        SoundManager.Instance.CreateSound().WithSoundData(m_triggerOn).WithRandomPitch().WithPosition(transform.position).Play();
         m_isTriggered.Value = true;
     }
     void OnTriggerExit2D(Collider2D other)
     {
         if((m_triggerLayers.value & (1 << other.gameObject.layer)) is 0){ return; }
+        SoundManager.Instance.CreateSound().WithSoundData(m_triggerOff).WithRandomPitch().WithPosition(transform.position).Play();
         m_isTriggered.Value = false;
     }
 }
