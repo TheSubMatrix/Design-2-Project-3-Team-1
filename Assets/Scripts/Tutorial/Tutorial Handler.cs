a5be94ac250b7709403f9947888aacaaf0acdc82
Colin Whiteford <colinwhiteford516@gmail.com>
2025-11-30 00:28:14 -0500

    Backend Updates to Tutorial System

--- Assets/Scripts/Tutorial/Tutorial Handler.cs
+++ Assets/Scripts/Tutorial/Tutorial Handler.cs
@@ -11,25 +11,28 @@ public class TutorialHandler : MonoBehaviour
     IPlayerMovementEventProvider m_playerMovement;
     [Inject]
     IBowEventProvider m_bow;
-    List<TutorialStep> m_steps = new();
+
+    readonly List<TutorialStep> m_steps = new();
 
     public void Awake()
     {
         m_steps.Clear();
 
-        m_steps.Add(TutorialStep.Create<IPlayerMovementEventProvider.OnMove>(
+        m_steps.Add(TutorialStepDelegate<IPlayerMovementEventProvider.OnMove>.Create(
             subscribe: (handler) => m_playerMovement.OnMoveEvent += handler,
             unsubscribe: (handler) => m_playerMovement.OnMoveEvent -= handler,
             onStepStarted: () => m_textDisplay.ShowTextCoroutine("Use WASD to move"),
             onStepEnded: () => m_textDisplay.HideTextCoroutine()
         ));
-        m_steps.Add(TutorialStep.Create<IBowEventProvider.OnBowCharge>(
+        
+        m_steps.Add(TutorialStepDelegate<IBowEventProvider.OnBowCharge>.Create(
             subscribe: (handler) => m_bow.OnBowChargeEvent += handler,
             unsubscribe: (handler) => m_bow.OnBowChargeEvent -= handler,
             onStepStarted: () => m_textDisplay.ShowTextCoroutine("Hold right click to charge"),
             onStepEnded: () => m_textDisplay.HideTextCoroutine()
         ));
-        m_steps.Add(TutorialStep.Create<IBowEventProvider.OnBowFire>(
+        
+        m_steps.Add(TutorialStepDelegate<IBowEventProvider.OnBowFire>.Create(
             subscribe: (handler) => m_bow.OnBowFireEvent += handler,
             unsubscribe: (handler) => m_bow.OnBowFireEvent -= handler,
             onStepStarted: () => m_textDisplay.ShowTextCoroutine("While holding right click, press left click to fire"),
@@ -46,7 +49,7 @@ public class TutorialHandler : MonoBehaviour
     {
         foreach (TutorialStep tutorialStep in m_steps)
         {
-            yield return tutorialStep.TutorialStepSequenceAsync();
+            yield return tutorialStep.ExecuteAsync();
         }
     }
 }
--- Assets/Scripts/Tutorial/TutorialStep.cs
+++ Assets/Scripts/Tutorial/TutorialStep.cs
@@ -1,89 +1,70 @@
 ﻿using System;
 using System.Collections;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
-using UnityEngine;
+using UnityEngine.Events;
 
-
-public class TutorialStep
+public abstract class TutorialStep
 {
     public bool IsRunning { get; private set; }
     
-    bool m_isCompleted;
-
-    Action m_enableListening;
-    Action m_disableListening;
+    protected bool IsCompleted;
 
     readonly Func<IEnumerator> m_onStepStarted;
     readonly Func<IEnumerator> m_onStepEnded;
         
-    private TutorialStep(Func<IEnumerator> onStepStarted, Func<IEnumerator> onStepEnded)
+    protected TutorialStep(Func<IEnumerator> onStepStarted, Func<IEnumerator> onStepEnded)
     {
         m_onStepStarted = onStepStarted;
         m_onStepEnded = onStepEnded;
     }
-    
-    ~TutorialStep()
-    {
-        m_disableListening?.Invoke();
-    }
 
-    private void Complete()
+    protected virtual void Complete()
     {
         if(IsRunning)
         {
-            m_isCompleted = true;
+            IsCompleted = true;
         }
     }
     
     public void ForceComplete()
     {
-        if (IsRunning && !m_isCompleted)
+        if (IsRunning && !IsCompleted)
         {
             Complete();
         }
     }
 
-    public IEnumerator TutorialStepSequenceAsync()
+    public IEnumerator ExecuteAsync()
     {
-        m_isCompleted = false;
+        IsCompleted = false;
         IsRunning = true;
+        
         if (m_onStepStarted != null)
             yield return m_onStepStarted.Invoke();
-        m_enableListening?.Invoke();
-        yield return new WaitUntil(() => m_isCompleted);
-        m_disableListening?.Invoke();
+        
+        OnStepStarted();
+        
+        yield return WaitForCompletion();
+        
+        OnStepEnded();
+        
         if (m_onStepEnded != null)
             yield return m_onStepEnded.Invoke();
         
         IsRunning = false;
     }
-    static TDelegate CreateHandler<TDelegate>(Action onComplete) where TDelegate : Delegate
-    {
-        MethodInfo invoke = typeof(TDelegate).GetMethod("Invoke");
-        ParameterInfo[] parameters = invoke?.GetParameters();
-        Expression<TDelegate> lambda = Expression.Lambda<TDelegate>(
-            Expression.Call(Expression.Constant(onComplete), nameof(Action.Invoke), null),
-            (parameters ?? Array.Empty<ParameterInfo>()).Select(p => Expression.Parameter(p.ParameterType, p.Name))
-        );
 
-        return lambda.Compile();
-    }
-    public static TutorialStep Create<T>(
-        Action<T> subscribe,
-        Action<T> unsubscribe,
-        Func<IEnumerator> onStepStarted,
-        Func<IEnumerator> onStepEnded)
-        where T : Delegate
-    {
-        TutorialStep step = new TutorialStep(onStepStarted, onStepEnded);
+    /// <summary>
+    /// Called when the step starts, before waiting for completion
+    /// </summary>
+    protected virtual void OnStepStarted() { }
 
-        T handler = CreateHandler<T>(step.Complete);
+    /// <summary>
+    /// Called when the step ends, after completion
+    /// </summary>
+    protected virtual void OnStepEnded() { }
 
-        step.m_enableListening = () => subscribe(handler);
-        step.m_disableListening = () => unsubscribe(handler);
-
-        return step;
-    }
+    /// <summary>
+    /// Returns the coroutine that waits for this step to complete
+    /// </summary>
+    protected abstract IEnumerator WaitForCompletion();
 }
